ac90c363777959784d19751c1a7f7d527dfbeff3
M.Satrya <satryabima24@gmail.com>
2013-02-14 21:26:31 +0700

    option id
    Signed-off-by: M.Satrya <satryabima24@gmail.com>

--- admin/options-framework.php
+++ admin/options-framework.php
@@ -87,7 +87,7 @@ function optionsframework_init() {
 		$option_name = $optionsframework_settings['id'];
 	}
 	else {
-		$option_name = 'tiga';
+		$option_name = 'options_framework_theme';
 	}
 	
 	// If the option has no saved data, load the defaults
